821f90d12c824459109a648f9d2b4bd936065e7e
MrNote11 <aliyahsulaiman3@gmail.com>
2025-03-13 23:35:56 -1100

    updated imagess

--- Home4U/Home4U/settings.py
+++ Home4U/Home4U/settings.py
@@ -195,15 +195,25 @@ USE_I18N = True
 
 USE_TZ = True
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Static Files Configuration
+STATIC_URL = '/static/'  # URL prefix for static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Development static folder
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Collects static files for production
+
+# Media Files Configuration (For Uploaded Images)
+MEDIA_URL = '/media/'  # URL prefix for media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Directory to store uploaded files
+
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Development static folder
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Development static folder
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
