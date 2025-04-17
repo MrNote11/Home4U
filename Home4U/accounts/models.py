955d9d3b31655b117015423e05568c3b4eb6c076
MrNote11 <aliyahsulaiman3@gmail.com>
2025-04-16 22:33:29 -1100

    complete at last6

--- Home4U/accounts/models.py
+++ Home4U/accounts/models.py
@@ -62,7 +62,8 @@ class VerificationToken(models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_image = models.ImageField(upload_to='user_images/', default='media/user_images/Good_things_are_coming.png')
+    profile_image = CloudinaryField(default='Good_things_are_coming_dok1ti', blank=True,
+                                        null=True)
 
     def __str__(self):
         return self.user.username
